1323fbfc8affbf7f730718b2b053eed6f017bb23
dangtus <dangtu9520@gmail.com>
2023-02-28 10:16:03 +0700

    them truong hop k co

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const utterance = new SpeechSynthesisUtterance();
 const bodyElement = document.querySelector(".chung-ta");
 
 window.onload = function () {
-    console.log("load");
-
     speechSynthesis.onvoiceschanged = loadVoices;
 };
 
 function loadVoices() {
+    if (speechSynthesis.getVoices().length == 0) {
+        bodyElement.innerHTML += `<p>không có cái nào</p>`;
+        return;
+    }
+
     for (let voice of speechSynthesis.getVoices()) {
         bodyElement.innerHTML += `<p>${voice.name}</p>`;
-        console.log(voice);
     }
 }
