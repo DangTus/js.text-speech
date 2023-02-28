2de170f642d782a6dbc9e6b9a83bc24fc2060e3b
dangtus <dangtu9520@gmail.com>
2023-02-28 11:02:03 +0700

    bat luc 3

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const utterance = new SpeechSynthesisUtterance();
 const bodyElement = document.querySelector(".chung-ta");
 
-let synth = window.speechSynthesis;
+const synth = window.speechSynthesis;
+
+if (synth.onvoiceschanged !== undefined) {
+    synth.onvoiceschanged = loadVoices;
+}
 
 function loadVoices() {
     bodyElement.innerHTML += `<h3>vao ham load voices</h3>`;
 
-    bodyElement.innerHTML += `<h2>Có ${
-        synth.getVoices().length
-    } ngôn ngữ</h2>`;
+    text = `Có ${synth.getVoices().length} ngôn ngữ`;
+
+    bodyElement.innerHTML += `<h2>${text}</h2>`;
 
     for (let voice of synth.getVoices()) {
         bodyElement.innerHTML += `<p>${voice.name}</p>`;
     }
+
+    utterance.text = text;
+    synth.speak(utterance);
 
     bodyElement.innerHTML += `<h3>ket thuc ham load voices</h3>`;
 }
