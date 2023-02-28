666c18fc11789bd8ecf3a61b1f3782bb47cc9f66
dangtus <dangtu9520@gmail.com>
2023-02-28 10:31:46 +0700

    fix code

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const utterance = new SpeechSynthesisUtterance();
 const bodyElement = document.querySelector(".chung-ta");
 
-window.onload = function () {
-    speechSynthesis.onvoiceschanged = loadVoices;
-};
+document.addEventListener("DOMContentLoaded", function (event) {
+    if (speechSynthesis.onvoiceschanged !== undefined) {
+        speechSynthesis.onvoiceschanged = loadVoices;
+    } else {
+        setTimeout(loadVoices, 100);
+    }
+});
 
 function loadVoices() {
-    bodyElement.innerHTML += `<h2>Có ${speechSynthesis.getVoices().length} ngôn ngữ</h2>`;
+    bodyElement.innerHTML += `<h2>Có ${
+        speechSynthesis.getVoices().length
+    } ngôn ngữ</h2>`;
 
     for (let voice of speechSynthesis.getVoices()) {
         bodyElement.innerHTML += `<p>${voice.name}</p>`;
